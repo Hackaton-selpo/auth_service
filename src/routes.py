520bd2f5f4910bda2b5189d6818d2a404170a9be
danek52 <guskir3565@gmail.com>
2025-05-22 16:57:26 +0300

    delete test ep

--- src/routes.py
+++ src/routes.py
@@ -5,8 +5,3 @@ from src.modules.reg_module.routes import router as auth_router
 
 main_router = APIRouter()
 main_router.include_router(auth_router)
-
-
-@main_router.post("/test")
-async def test(_: None = Depends(_get_access_token_from_headers)):
-    pass
